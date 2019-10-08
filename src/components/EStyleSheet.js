459c2cadaa22437d81dae62344cec5490d52d3bd
S.M.A. Javadi <djawadi@riseup.net>
2019-10-08 22:18:21 +0330

    Add Vazir font

--- src/components/EStyleSheet.js
+++ src/components/EStyleSheet.js
@@ -13,56 +13,21 @@ EStyleSheet.build({
   $height: height,
   ...colors,
   $f: {
-    fontFamily: 'Yekan Bakh FA 04 Regular',
+    fontFamily: 'Vazir',
     fontWeight: 'normal',
   },
   '@media android': {
     $fb: {
-      fontFamily: 'Yekan Bakh FA 06 Bold',
+      fontFamily: 'Vazir-Bold',
       fontWeight: '800',
     },
     $fm: {
-      fontFamily: 'Yekan Bakh FA 05 Medium',
+      fontFamily: 'Vazir-Medium',
       fontWeight: '500',
     },
   },
 
   // '@media ios': {
-  //   $fd: {
-  //     fontFamily: 'IRANSansMobile(FaNum)',
-  //     fontWeight: 'normal',
-  //   },
-  //   $fb: {
-  //     // Has Bold Font
-  //     fontFamily: 'IRANSansMobile',
-  //     fontWeight: '700',
-  //   },
-  //   $fl: {
-  //     // Has Light Font
-  //     fontFamily: 'IRANSansMobile',
-  //     fontWeight: '100',
-  //   },
-  //   $fm: {
-  //     fontFamily: 'IRANSansMobile',
-  //     fontWeight: '500',
-  //   },
-  //   $fbd: {
-  //     // Has Bold Font
-  //     fontFamily: 'IRANSansMobile(FaNum)',
-  //     fontWeight: '700',
-  //   },
-  //   $fld: {
-  //     // Has Light Font
-  //     fontFamily: 'IRANSansMobile(FaNum)',
-  //     fontWeight: '100',
-  //   },
-  //   $fmd: {
-  //     fontFamily: 'IRANSansMobile(FaNum)',
-  //     fontWeight: '500',
-  //   },
-  //   $fdigits: {
-  //     fontFamily: 'KreditBack-Regular',
-  //   },
-  // },
+  // }
 });
 export default EStyleSheet;
